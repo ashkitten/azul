6696a6a38d131deb172a918fd4cff9afc13aa798
Norbert Nemec <Norbert@Nemec-online.de>
2020-01-05 17:54:07 +0100

    adjust image demo to compile with latest azul

--- examples/image/image.rs
+++ examples/image/image.rs
@@ -3,7 +3,7 @@ use azul::{prelude::*, widgets::button::Button};
 struct GlitchApp {}
 
 impl Layout for GlitchApp {
-    fn layout(&self, info: LayoutInfo<Self>) -> Dom<Self> {
+    fn layout(&self, info: LayoutInfo) -> Dom<Self> {
         let button = Button::with_label("Load Image")
             .dom()
             .with_callback(On::MouseUp, load_image);
@@ -20,18 +20,13 @@ impl Layout for GlitchApp {
 
 fn load_image(info: CallbackInfo<GlitchApp>) -> UpdateScreen {
     if let Some(path) = azul::dialogs::open_file_dialog(None, None) {
-        let image_id = info.state.resources.add_css_image_id("preview_image");
-        info.state
-            .resources
-            .add_image_source(image_id, ImageSource::File(path.into()));
+        let image_id = info.resources.add_css_image_id("preview_image");
+        info.resources.add_image_source(image_id, ImageSource::File(path.into()));
     }
     Redraw
 }
 
 fn main() {
-    let mut app = App::new(GlitchApp {}, AppConfig::default()).unwrap();
-    let window = app
-        .create_window(WindowCreateOptions::default(), css::native())
-        .unwrap();
-    app.run(window).unwrap();
+    let app = App::new(GlitchApp { }, AppConfig::default()).unwrap();
+    app.run(WindowCreateOptions::new(css::native()));
 }
